1736884e036ae9583a442355925c7cd05ab9ab88
Ben-hur <mailem.meldavid@gmail.com>
2025-09-20 13:25:29 +0800

    feat: Update package.json and vite.config.ts for project configuration and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/meruuuuooo/SQLFormatter.git' // 👈 replace with your repo name
+  base: '/SQLFormatter/',
 })
